0933879b892c7ac1830f34c56d826fbaa7aed543
TehBrian <tehbrian@proton.me>
2024-08-11 12:14:15 -0400

    paper-plugin: update deps; mc 1.21.1; relocate libs fully

--- fragments/java_paper_plugin/build.gradle.kts
+++ fragments/java_paper_plugin/build.gradle.kts
@@ -1,8 +1,9 @@
 plugins {
     id("java")
-    id("com.github.johnrengelman.shadow") version "7.1.2"
-    id("xyz.jpenilla.run-paper") version "1.0.6"
-    id("net.kyori.indra.checkstyle") version "2.2.0"
+    id("com.github.johnrengelman.shadow") version "8.1.1"
+    id("xyz.jpenilla.run-paper") version "2.3.0"
+    id("net.kyori.indra.checkstyle") version "3.1.3"
+    id("com.github.ben-manes.versions") version "0.51.0"
 }
 
 group = "@PROJECT_GROUP@"
@@ -10,18 +11,17 @@ version = "@PROJECT_VERSION@"
 description = "@PROJECT_DESCRIPTION@"
 
 java {
-    toolchain.languageVersion.set(JavaLanguageVersion.of(17))
+    toolchain.languageVersion.set(JavaLanguageVersion.of(21))
 }
 
 repositories {
     mavenCentral()
-    maven("https://papermc.io/repo/repository/maven-public/") {
-        name = "papermc"
-    }
+    maven("https://papermc.io/repo/repository/maven-public/")
+
 }
 
 dependencies {
-    compileOnly("io.papermc.paper:paper-api:1.19-R0.1-SNAPSHOT")
+    compileOnly("io.papermc.paper:paper-api:1.21.1-R0.1-SNAPSHOT")
 }
 
 tasks {
@@ -30,18 +30,35 @@ tasks {
     }
 
     processResources {
-        expand("version" to project.version, "description" to project.description)
+        filesMatching("plugin.yml") {
+            expand(
+                "version" to project.version,
+                "description" to project.description
+            )
+        }
     }
 
+    base {
+		archivesName.set("@PROJECT_NAME@")
+	}
+
     shadowJar {
-        archiveBaseName.set("@PROJECT_NAME@")
         archiveClassifier.set("")
 
         val libsPackage = "${project.group}.${project.name}.libs"
-        relocate("the.developer.epiclib", "$libsPackage.epiclib")
+        fun moveToLibs(vararg patterns: String) {
+            for (pattern in patterns) {
+                relocate(pattern, "$libsPackage.$pattern")
+            }
+        }
+
+        moveToLibs(
+            "love.broccolai.corn",
+            "dev.tehbrian.tehlib",
+        )
     }
 
     runServer {
-        minecraftVersion("1.19")
+        minecraftVersion("1.21.1")
     }
 }
